5e3844ebc342712d5ed3545254fe0318b68e9d73
Aditya Kadam <79622885+adii-tech@users.noreply.github.com>
2023-09-11 23:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
   // your code here
+	let s=text.trim();
+	return s.charAt(0);
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const text = prompt("Enter text:");
 alert(firstChar(text));
+let tr=document.getElementbyId("tr");
+
+let td=document.createElement("td");
+let textNode=document.createTextNode(`firstChar('${text}')`);
+tr.appendChild(td);
